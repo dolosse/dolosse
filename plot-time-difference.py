f2dbab57990457dd95414c27577ecbcf9a53da97
S.V. Paulauskas <stanpaulauskas@gmail.com>
2018-12-31 14:36:41 -0500

    updating logbook and prepping to change computers

--- plot-time-difference.py
+++ plot-time-difference.py
@@ -1,6 +1,7 @@
 import psycopg2
 import pandas as pd
 import threading
+import time
 import yaml
 
 
@@ -22,22 +23,22 @@ def chunk_data_frame(frame, n):
 
 def run(conn, frame):
     cursor = connection.cursor()
-    for idx, trigger in frame.iterrows():
-        coin_low = int(trigger) - 126
-        coin_high = int(trigger) - 63
-        statement = 'select * from id10 where time between ' + str(coin_low) + ' and ' + str(coin_high)
-        cursor.execute(statement)
-        df = pd.DataFrame.from_dict(cursor.fetchall())
-        if not df.empty:
-            df['trigger'] = trigger
-            df.columns = ['energy', 'time', 'trigger']
-            df['diff'] = trigger - df['time']
-            print(threading.currentThread().getName(), ':\n', df)
+    start = time.time()
+    print(threading.current_thread().name, '- Generating statement list.')
+    statement = ''
+    for row in frame.itertuples():
+        statement += cursor.mogrify(
+            "insert into gated select id, energy, time from data where time between %s and %s and id>3;",
+            (str(row[1] - 126), str(row[1] - 63))).decode()
+    print(threading.currentThread().name, 'Finished generating statement list in ', time.time() - start, 'seconds.')
+    start = time.time()
+    print(threading.current_thread().name, '- Executing many.')
+    cursor.execute(statement)
+    print(threading.current_thread().name, '- Finished executing many in ', time.time() - start, 'seconds.')
 
 
 if __name__ == '__main__':
     with open('cfg.yaml') as f:
-        # use safe_load instead load
         cfg = yaml.safe_load(f)
 
     try:
@@ -47,14 +48,16 @@ if __name__ == '__main__':
         cursor.execute('select time from id00;')
         triggers = pd.DataFrame.from_dict(cursor.fetchall())
         threads = []
-        num_threads = 10
+        num_threads = cfg['numThreads']
         framechunks = chunk_data_frame(triggers, num_threads)
+        start = time.time()
         for i in range(0, num_threads):
             t = threading.Thread(target=run, args=(connection, framechunks[i]))
             threads.append(t)
             t.start()
         for thread in threads:
             thread.join()
+        print('Main Thread - Executed in ', (time.time() - start)/60, " minutes.")
     except psycopg2.Error as ex:
         print("Error connecting to db.", ex)
 
