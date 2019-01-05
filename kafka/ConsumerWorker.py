e585decfa6f1f2d8afa8ff0a5451ac2d957b08ef
S.V. Paulauskas <stanpaulauskas@gmail.com>
2019-01-05 12:03:46 -0500

    Adding logging to the program

--- kafka/ConsumerWorker.py
+++ kafka/ConsumerWorker.py
@@ -1,39 +1,51 @@
 from confluent_kafka import Consumer, KafkaError
+import logging
 import threading
-import json
 import time
 
-
 class ConsumerWorker(threading.Thread):
-    def __init__(self, cfg):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, cfg, name):
         threading.Thread.__init__(self)
 
         self.shutdown_flag = threading.Event()
+        self.handled = False
         self.cfg = cfg
-        self.consumer = Consumer({'group.id': cfg['consumer_group'],
-                                  'default.topic.config': {'auto.offset.reset': 'earliest'},
-                                  'auto.commit.interval.ms': 500})
+        try:
+            self.consumer = Consumer({'bootstrap.servers': cfg['consumer']['bootstrap_servers'],
+                                  'group.id': cfg['consumer']['group'],
+                                  'default.topic.config': {'auto.offset.reset': cfg['consumer']['auto_offset_reset']},
+                                  'auto.commit.interval.ms': cfg['consumer']['auto_commit_interval_ms']})
+        except Exception as ex:
+            pass
 
     def run(self):
-        print("Thread start at", time.time())
-        self.consumer.subscribe([self.cfg['topic']])
+        logging.info("Started")
+        self.consumer.subscribe([self.cfg['consumer']['topic']])
 
-        timeouts = 0
-        records = 0
-        while not self.shutdown_flag.is_set():
-            msg = self.consumer.poll(timeout=0.200)
-            print(self.getName(), msg)
+        timeouts = records = 0
+        stats_interval_start_time = time.time()
+        while not self.shutdown_flag.is_set() or self.handled:
+            msg = self.consumer.poll(timeout=self.cfg['consumer']['poll_timeout_s'])
             if msg is None:
                 timeouts += 1
-                continue
-            if not msg.error():
+            elif not msg.error():
                 records += 1
                 print("Got {records} records after {timeouts} timeouts".format(records=records, timeouts=timeouts))
                 timeouts = 0
                 records = 0
-                msg_value = json.loads(msg.value())
-                print(json.dumps(msg_value))
+                print(msg.value().decode())
             elif msg.error().code() != KafkaError._PARTITION_EOF:
                 print(msg.error())
+
+            elapsed_time = time.time() - stats_interval_start_time
+            if elapsed_time >= self.cfg['consumer']['stats_interval_s']:
+                logging.info("Thread status update.")
+                stats_interval_start_time = time.time()
+            elif self.shutdown_flag.is_set():
+                logging.info("Thread received the shutdown signal, wrapping up...")
+            elif self.handled:
+                logging.warning("Thread became inactive for some reason, check logs for more information!")
         self.consumer.close()
-        print("Thread end at", time.time())
+        logging.info("Thread end at" + str(time.time()))
--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,4 +1,5 @@
 import logging
+from logging.config import dictConfig
 import signal
 import time
 import yaml
@@ -15,14 +16,33 @@ class ServiceExit(Exception):
 
 
 def service_shutdown(signum, frame):
-    print('Caught signal %d' % signum)
+    logging.info('Caught shutdown signal %d' % signum)
     raise ServiceExit
 
 
+def shutdown_threads(threads):
+    for thread in threads:
+        thread.shutdown_flag.set()
+        thread.join()
+
+
 if __name__ == '__main__':
-    print('Starting main program')
-    with open('consumer.yaml') as f:
-        cfg = yaml.safe_load(f)
+    logging.info('Main thread start')
+    try:
+        with open('consumer.yaml') as f:
+            cfg = yaml.safe_load(f)
+    except yaml.scanner.ScannerError as err:
+        logging.exception('consumer.py ran into an issue loading the YAML config.')
+        exit(1)
+    except ValueError as err:
+        logging.exception('consumer.py caught a value error while loading the config')
+        exit(1)
+
+    try:
+        logging.config.dictConfig(cfg['logging'])
+    except Exception as ex:
+        logging.exception('Caught exception while setting up logger')
+        exit(1)
 
     # Register the signal handlers
     signal.signal(signal.SIGABRT, service_shutdown)
@@ -30,21 +50,26 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, service_shutdown)
 
     threads = []
-    num_threads = cfg['numThreads']
     start = time.time()
     try:
-        for i in range(0, num_threads):
-            t = ConsumerWorker(cfg=cfg)
+        for i in range(0, cfg['consumer']['number_of_threads']):
+            t = ConsumerWorker(cfg=cfg, name="ConsumerThread-"+str(i))
             t.setDaemon(True)
             t.start()
             threads.append(t)
         while True:
             try:
+                if len(threads) > 0:
+                    for t in threads:
+                        if not t.isAlive():
+                            # get results from thread
+                            t.handled = True
+                    threads = [t for t in threads if not t.handled]
+                else:
+                    logging.critical("All threads have died.")
+                    break
                 time.sleep(0.5)
             except InterruptedError:
                 pass
     except ServiceExit:
-        for thread in threads:
-            thread.shutdown_flag.set()
-            thread.join()
-    print("Done!")
+        shutdown_threads(threads)
