cae03e4f3a477169b8fdab673e402354b8bfd84c
S.V. Paulauskas <stanpaulauskas@gmail.com>
2019-01-12 09:37:49 -0500

    Initial commit of the code for reading binary data files

---
+++ kafka/binary-test.py
@@ -0,0 +1,50 @@
+import struct
+import io
+import time
+
+dir_block = b'DIR '
+head_block = b'HEAD'
+data_block = b'DATA'
+end_of_file = b'EOF '
+buffer_padding = b'\xff\xff\xff\xff'
+
+files = ['D:/data/svp/kafka-tests/bagel-single-spill.pld',
+         'D:/data/svp/kafka-tests/kafka-data-test-0.pld',
+         'D:/data/utk/pixieworkshop/pulser_003.ldf']
+
+for file in files:
+    print("Working on file: ", file)
+    num_data_blocks = num_end_of_file = num_buffer_padding = num_head_blocks = num_other_stuff = total_words = \
+        read_start_time = read_end_time = num_dir_blocks = 0
+    with io.open(file, 'rb') as f:
+        read_start_time = time.time()
+        while True:
+            chunk = f.read(4)
+            total_words += 1
+            if chunk:
+                if chunk == data_block:
+                    num_data_blocks += 1
+                elif chunk == dir_block:
+                    num_dir_blocks += 1
+                elif chunk == end_of_file:
+                    num_end_of_file += 1
+                elif chunk == buffer_padding:
+                    num_buffer_padding += 1
+                elif chunk == head_block:
+                    num_head_blocks +=1
+                else:
+                    num_other_stuff += 1
+                    # print(chunk, struct.unpack('i', chunk)[0])
+            else:
+                break
+        read_end_time = time.time()
+        print("Basic statistics for the file:",
+              "\n\tNumber of Dirs         : ", num_dir_blocks,
+              "\n\tNumber of Head         : ", num_head_blocks,
+              "\n\tNumber of Data Blocks  : ", num_data_blocks,
+              "\n\tNumber of Buffer Pads  : ", num_buffer_padding,
+              "\n\tNumber of End of Files : ", num_end_of_file,
+              "\n\tTotal Words in File    : ", total_words,
+              "\n\tTime To Read (s)       : ", read_end_time - read_start_time,)
+
+    f.close()
