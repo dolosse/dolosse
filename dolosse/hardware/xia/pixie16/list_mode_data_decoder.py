e2ae9da4be0e15da1dd9b76a651574cd61938b0f
S.V. Paulauskas <stanpaulauskas@gmail.com>
2019-05-22 20:25:03 -0400

    Updating decoder tests and rewriting the decoder.

--- dolosse/hardware/xia/pixie16/list_mode_data_decoder.py
+++ dolosse/hardware/xia/pixie16/list_mode_data_decoder.py
@@ -29,21 +29,43 @@ def decode_word_zero(word, mask):
     }
 
 
-def decode_word_one(word, mask):
-    """
-    All Pixie16 firmwares use Word 1 as the low 32-bits of the time stamp
-    :param word: The word that we're not going to do anything with.
-    :return: The input word
-    """
-    return word
-
-
 def decode_word_two(word, mask):
-    print(word)
+    """
+    Decodes the
+       * high component of the event time
+       * CFD time
+       * CFD Trigger source bit
+       * CFD forced trigger bit
+    :param word: The word that we're going to decode.
+    :param mask: The mask we'll use to decode that word.
+    :return: A dictionary containing the decoded information
+    """
+    return {
+        'event_time_high': (word & mask.event_time_high()[0]) >> mask.event_time_high()[1],
+        'cfd_fractional_time': (word & mask.cfd_fractional_time()[0]) >>
+                               mask.cfd_fractional_time()[1],
+        'cfd_trigger_source_bit': (word & mask.cfd_trigger_source()[0]) >>
+                                  mask.cfd_trigger_source()[1],
+        'cfd_forced_trigger_bit': (word & mask.cfd_forced_trigger()[0]) >>
+                                  mask.cfd_forced_trigger()[1]
+    }
 
 
 def decode_word_three(word, mask):
-    print(word)
+    """
+    The final word of the standard header. Contains
+        * trapezoidal energy
+        * trace length
+        * trace out of range flag
+    :param word: The word that we're going to decode.
+    :param mask: The mask we'll use to decode that word.
+    :return: A dictionary containing the decoded information
+    """
+    return {
+        'energy': (word & mask.energy()[0]) >> mask.energy()[1],
+        'trace_length': (word & mask.trace_length()[0]) >> mask.trace_length()[1],
+        'trace_out_of_range': (word & mask.trace_out_of_range()[0]) >> mask.trace_out_of_range()[1]
+    }
 
 
 def decode_external_timestamps():
@@ -82,34 +104,15 @@ class ListModeDataDecoder(threading.Thread):
     def run(self):
         """ Decodes data from Pixie16 binary data stream """
         # TODO : Will need to have it create a list of the dictionaries and return that list.
+        decoded_data_list = []
         for chunk in iter(partial(self.stream.read, data.WORD), b''):
-            dict0 = decode_word_zero(struct.unpack('I', chunk)[0], self.mask)
-            dict1 = decode_word_one(struct.unpack('I', self.stream.read(data.WORD))[0], self.mask)
-            dict2 = decode_word_two(struct.unpack('I', self.stream.read(data.WORD))[0], self.mask)
-            dict3 = decode_word_three(struct.unpack('I', self.stream.read(data.WORD))[0], self.mask)
-
-            decoded_data = {
-                'channel': (word0 & self.mask.channel()[0]) >> self.mask.channel()[1],
-                'slot': (word0 & self.mask.slot()[0]) >> self.mask.slot()[1],
-                'crate': (word0 & self.mask.crate()[0]) >> self.mask.crate()[1],
-                'header_length': (word0 & self.mask.header_length()[0]) >>
-                                 self.mask.header_length()[1],
-                'event_length': (word0 & self.mask.event_length()[0]) >>
-                                self.mask.event_length()[1],
-                'finish_code': (word0 & self.mask.finish_code()[0]) >> self.mask.finish_code()[1],
-                'event_time_low': word1,
-                'event_time_high': (word2 & self.mask.event_time_high()[0]) >>
-                                   self.mask.event_time_high()[1],
-                'cfd_fractional_time': (word2 & self.mask.cfd_fractional_time()[0]) >>
-                                       self.mask.cfd_fractional_time()[1],
-                'cfd_trigger_source_bit': (word2 & self.mask.cfd_trigger_source()[0]) >>
-                                          self.mask.cfd_trigger_source()[1],
-                'cfd_forced_trigger_bit': (word2 & self.mask.cfd_forced_trigger()[0]) >>
-                                          self.mask.cfd_forced_trigger()[1],
-                'energy': (word3 & self.mask.energy()[0]) >> self.mask.energy()[1],
-                'trace_length': (word3 & self.mask.trace_length()[0])
-                                >> self.mask.trace_length()[1],
-                'trace_out_of_range': (word3 & self.mask.trace_out_of_range()[0])
-                                      >> self.mask.trace_out_of_range()[1]
-            }
+            decoded_data = decode_word_zero(struct.unpack('I', chunk)[0], self.mask)
+            decoded_data.update({
+                'event_time_low': struct.unpack('I', self.stream.read(data.WORD))[0],
+            })
+            decoded_data.update(
+                decode_word_two(struct.unpack('I', self.stream.read(data.WORD))[0], self.mask))
+            decoded_data.update(
+                decode_word_three(struct.unpack('I', self.stream.read(data.WORD))[0], self.mask))
+            print(decoded_data)
         self.finished = True
--- dolosse/hardware/xia/pixie16/tests/list_mode_data_decoder_tests.py
+++ dolosse/hardware/xia/pixie16/tests/list_mode_data_decoder_tests.py
@@ -24,6 +24,21 @@ class ListModeDataDecoderTestCase(unittest.TestCase):
             'finish_code': 0
         }, decoder.decode_word_zero(540717, self.mask))
 
+    def test_decode_word_two(self):
+        self.assertEqual({
+            'event_time_high': 26001,
+            'cfd_fractional_time': 0,
+            'cfd_trigger_source_bit': 0,
+            'cfd_forced_trigger_bit': 0
+        }, decoder.decode_word_two(26001, self.mask))
+
+    def test_decode_word_three(self):
+        self.assertEqual({
+            'energy': 2345,
+            'trace_length': 0,
+            'trace_out_of_range': 0
+        }, decoder.decode_word_three(2345, self.mask))
+
 
 if __name__ == '__main__':
     unittest.main()
