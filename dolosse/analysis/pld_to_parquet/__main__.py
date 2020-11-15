38703899634d15747b93206b915c73d12e10fbc2
S. V. Paulauskas <stanpaulauskas@gmail.com>
2020-11-15 17:58:03 -0500

    118: Adding import so we get execution right

--- dolosse/analysis/pld_to_parquet/__main__.py
+++ dolosse/analysis/pld_to_parquet/__main__.py
@@ -1,3 +1,11 @@
+"""
+file: __main__.py
+brief: Defines the main module for pld_to_parquet
+author: S. V. Paulauskas
+date: November 15, 2020
+"""
+from dolosse.analysis.pld_to_parquet import pld_to_parquet
+
 try:
     pld_to_parquet()
 except KeyboardInterrupt:
